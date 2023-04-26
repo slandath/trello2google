6b3c9adcece3d091dea0c724c46cd1f8533b6006
slandath <jesus_slanda@hotmail.com>
2023-04-25 21:26:00 -0400

    remove gitignore files

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ app.get("/success", async (req, res) => {
   res.send("Success!");
 });
 
+app.get("/", async (req, res) => {
+  res.send("Nothing here");
+});
+
 app.listen(PORT, () => console.log("Server running on Port " + PORT));
